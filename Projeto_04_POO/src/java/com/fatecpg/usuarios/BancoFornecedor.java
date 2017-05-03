a86bb3680db5e766e5510e391520f10370dcf52e
Ricky <Ricky@vanricky>
2017-05-02 23:53:44 -0300

    Enviando Paginas e classe sem codigo

--- Projeto_04_POO/src/java/com/fatecpg/usuarios/BancoFornecedor.java
+++ Projeto_04_POO/src/java/com/fatecpg/usuarios/BancoFornecedor.java
@@ -5,10 +5,18 @@
  */
 package com.fatecpg.usuarios;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Ricky
  */
 public class BancoFornecedor {
-    
+
+    private static HashMap<String, Fornecedor> f;
+
+    public static HashMap<String, Fornecedor> Usar() {
+        f = new HashMap<>();
+        return f;
+    }
 }
